2fc1fe489a0f63897fdf05f35cd304df082241b9
saminoorsyed <saminoorsyed@gmail.com>
2022-08-07 13:43:18 -0400

    Finish Ex 11 -- adjusting html5 Player
    Feeling like I'm getting the hang of Dom manipulation and javascript
    syntax:
    <script>
    // select an element;
    // listen for an event;
    //run a function that adjusts Dom properties;
    </script>.
    getting curious about how to discover properties of existing elements
    used event flags to tell whether mouse is being held down on drag
    logged events to get progress of video and adjust based on click
    location

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,12 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
-const skipButts = player.querySelectorAll('[data-skip]')
+const skipButts = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const progressBar = player.querySelector('.progress__filled');
+const progress = player.querySelector('.progress');
+let rangeDrag = false;
+let progressDrag = false;
 
 function togglePlay(){
     video[video.paused ? 'play' : 'pause']();
@@ -17,8 +22,45 @@ function skipper(){
     // console.log(this.dataset) <- gives every "data-"" attribute's value
     video.currentTime += parseFloat(this.dataset.skip)
 }
+
+function handleSlider(){
+    if (!rangeDrag) return
+    video[this.name] = this.value;
+    // don't do it this way -->
+    // if (this.name === 'volume'){
+
+    // }
+    // if (this.name === 'playbackRate'){
+
+    // }
+}
+
+function handleProgress(){
+    const percent = (video.currentTime/video.duration) *100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime
+    
+}
+
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => progressDrag && scrub(e));
+progress.addEventListener('mousedown', () => progressDrag = true);
+progress.addEventListener('mouseup', () => progressDrag = false);
+
+
 skipButts.forEach(button => button.addEventListener('click',skipper));
+ranges.forEach(slider => slider.addEventListener('change', handleSlider));
+ranges.forEach(slider => slider.addEventListener('mousemove', handleSlider));
+ranges.forEach(slider => slider.addEventListener('mousedown',() => rangeDrag = true));
+ranges.forEach(slider => slider.addEventListener('mouseup',() => rangeDrag = false));
+ranges.forEach(slider => slider.addEventListener('mouseout',() => rangeDrag = false));
